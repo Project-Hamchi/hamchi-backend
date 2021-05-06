d47bb0dc5f628f1a85c8da4c7144a04628e904a0
hyeongju <kokiguitars@gmail.com>
2021-05-07 00:46:19 +0900

    Add authenticate user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,29 @@
+const createError = require('http-errors');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.JWT_SECRET;
+
+const User = require('../models/User');
+
+exports.signin = async function (req, res, next) {
+  try {
+    const { email, password } = req.body;
+    const currentUser = User.findOne({ email }).lean();
+
+    if (!currentUser) {
+      return next(createError(400, 'user not exist'));
+    }
+
+    const isCorrectPassword = argon2.verify(currentUser.password, password);
+    if (!isCorrectPassword) {
+      return next(createError(403, 'wrong password'));
+    }
+
+    const appIdToken = jwt.sign(currentUser._id, secretKey);
+
+    return res.json({ currentUser, appIdToken });
+  } catch (err) {
+    next(createError(500, err));
+  }
+
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/signin', userController.signin);
 
 module.exports = router;
