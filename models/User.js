67c27248a1c681da1bb0805535dd0f0424dec68b
hyeongju <kokiguitars@gmail.com>
2021-05-06 21:42:22 +0900

    Add User schema model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    minLength: 8,
+    trim: true,
+    required: true,
+  },
+  profileImage: {
+    type: String,
+  },
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+  }],
+  submissions: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Submission',
+  }],
+  chattings: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Chat',
+  }],
+});
+
+module.exports = mongoose.model('User', userSchema);
